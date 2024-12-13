34e2e2ae4f62361eeb1171472d872f84e1dd5303
chinmoy50 <chinmoy@intruceptlabs.com>
2024-12-13 17:46:49 +0530

    first commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "GzipSwift",
+    products: [
+        .library(name: "Gzip", targets: ["Gzip"]),
+    ],
+    targets: [
+        .target(
+            name: "Gzip",
+            dependencies: ["system-zlib"]
+        ),
+        .target(
+            name: "system-zlib"
+        ),
+        .testTarget(
+            name: "GzipTests",
+            dependencies: ["Gzip"],
+            resources: [.copy("test.txt.gz")]
+        ),
+    ],
+    swiftLanguageVersions: [.v6]
+)
